199c22b8f45e2e725e93e7be7bf1c2801cf079e7
Aleksander Sardara <86119224+galactic1106@users.noreply.github.com>
2024-05-28 23:37:08 +0200

    added threading to main

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 #quando viene importata la directory che lo contiene 
 #verranno eseguite tutte funzioni contenute in esso 
 from webapp import create_app 
+import threading
 
 app = create_app()
 
 if __name__=='__main__':
-    app.run()
+    on_colab=False
+    if on_colab:
+        threading.Thread(target=app.run, kwargs={"use_reloader": False}).start()
+    else
+        app.run()
