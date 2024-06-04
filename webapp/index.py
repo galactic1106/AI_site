d42fff7b3e5921aa11023fb7c919c810a8f996a5
Aleksander Sardara <86119224+galactic1106@users.noreply.github.com>
2024-06-04 16:32:11 +0200

    added functionality to gpt-2(non tested), may have broken dolly(not tested)

--- webapp/index.py
+++ webapp/index.py
@@ -4,11 +4,13 @@ from .gpt_2 import init_gpt_2, prompt_gpt_2
 from .dolly_3b import prompt_dolly_3b
 
 index = Blueprint("index", __name__)
-init_gpt_2=True
-gpt_2_ai=init_gpt_2()
+init_gpt_2 = True
+gpt_2_ai = init_gpt_2()
 
-init_dolly_3b=False
-if init_dolly_3b: prompt_dolly_3b('')
+init_dolly_3b = False
+if init_dolly_3b:
+    prompt_dolly_3b("")
+
 
 @index.route("/")
 def home():
@@ -17,7 +19,29 @@ def home():
 
 @index.route("/gpt-2")
 def gpt_2():
-    return render_template("gpt-2.html")
+    AI_response = ""
+    q_a = []
+    data = request.form
+    prompt = data.get("prompt")
+    # se non c'è un prompt
+    if prompt == None:
+        return render_template("gpt-2.html", q_a=q_a)
+    if len(prompt.strip()) < 0:
+        return render_template("gpt-2.html", q_a=q_a)
+
+    AI_response = prompt_gpt_2(gpt_2_ai, data.get("prompt"))
+    # AI_response = 'PLACEHOLDER'
+    try:
+        q_a = eval(data.get("q_a"))
+        q_a.append({"q": prompt, "a": AI_response})
+    except:
+        q_a = []
+
+    
+    #    for qa in q_a:
+    #        print(qa["q"])
+    #        print(qa["a"])
+    return render_template("gpt-2.html", q_a=q_a)
 
 @index.route("/guida")
 def guida():
@@ -36,16 +60,15 @@ def dolly_3b():
     if len(prompt.strip()) < 0:
         return render_template("dolly-3b.html", q_a=q_a)
 
-
     AI_response = prompt_dolly_3b(data.get("prompt"))
-    AI_response = 'PLACEHOLDER'
-    try: q_a = eval(data.get("q_a"))
-    except: q_a = []
-    
-    q_a.append({"q": prompt, "a": AI_response})
-#    for qa in q_a:
-#        print(qa["q"])
-#        print(qa["a"])
+    try:
+        q_a = eval(data.get("q_a"))
+        q_a.append({"q": prompt, "a": AI_response})
+    except:
+        q_a = []
+    #    for qa in q_a:
+    #        print(qa["q"])
+    #        print(qa["a"])
     return render_template("dolly-3b.html", q_a=q_a)
 
 
